5a3f8be6843ba525ed66fb214cab5e48f0de68eb
Samuel Steele <ssteele@bizstream.com>
2021-06-23 13:19:38 -0400

    add breadcrumbs to meta-package

---
+++ src/AspNetCore/Components/src/IServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BizStream.Kentico.Xperience.AspNetCore.Components.Breadcrumbs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bizstream.Kentico.Xperience.AspNetCore.Components
+{
+
+    /// <summary> Extensions to <see cref="IServiceCollection"/> for registering services required by Xperience Mvc Components. </summary>
+    public static class IServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddXperienceComponents( this IServiceCollection services )
+        {
+            if( services == null )
+            {
+                throw new ArgumentNullException( nameof( services ) );
+            }
+
+            services.AddXperienceBreadcrumbsComponent();
+
+            return services;
+        }
+
+    }
+
+}
