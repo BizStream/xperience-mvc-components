7be0b03a9f6225b29e86d505117a4604f1df7adc
Samuel Steele <ssteele@bizstream.com>
2021-07-13 22:51:58 -0400

    increment versions and rename CacheKeys static class

--- src/AspNetCore/Breadcrumbs/src/BreadcrumbCacheKeys.cs
+++ src/AspNetCore/Breadcrumbs/src/BreadcrumbCacheKeys.cs
@@ -1,16 +1,18 @@
 ﻿using BizStream.Kentico.Xperience.AspNetCore.Components.Breadcrumbs.Abstractions;
 using CMS.DocumentEngine;
+using Kentico.Content.Web.Mvc;
 
 namespace BizStream.Kentico.Xperience.AspNetCore.Components.Breadcrumbs
 {
 
     /// <summary> Static Helpers for generating cache keys. </summary>
-    public static class CacheKeys
+    public static class BreadcrumbCacheKeys
     {
 
         /// <summary> Creates a cache key for the nodes used to create breadcrumb items. </summary>
         /// <param name="node"> The node for which breadcrumb nodes are being cached. </param>
         /// <param name="options"> The options used to retrieve breadcrumb nodes. </param>
+        /// <remarks> Kentico's <see cref="IPageCacheBuilder{TPageType}"/> and <see cref="IPageCacheDependencyBuilder{TPageType}"/> handle culture and site specific cache key variance. </remarks>
         public static string Nodes( TreeNode node, BreadcrumbRetrievalOptions options )
             => $"breadcrumbnodes|{node.NodeAliasPath}|{options.IsCurrentPageIncluded}|{options.IsHomePageIncluded}";
 
